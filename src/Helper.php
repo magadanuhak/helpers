4421848dc3ef325efc92e59096ebeab8a84e2edb
Zelinschi Alexandr <alexandr.zelinschi@gmail.com>
2020-04-05 18:56:51 +0300

    add ServiceProvider, some settings to composer.json

--- src/Helper.php
+++ src/Helper.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Merax;
+
 class Helper
 {
     /**
---
+++ src/HelpersServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Merax;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+
+class HelpersServiceProvider extends ServiceProvider
+{
+
+    public function boot(): void
+    {
+        $helpersPath = __DIR__ . '/../Helpers/features';
+        foreach (scandir($helpersPath) as $helperFile) {
+            $path = sprintf(
+                '%s%s%s%s%s',
+                __DIR__,
+                DIRECTORY_SEPARATOR,
+                '../Helpers/features',
+                DIRECTORY_SEPARATOR,
+                $helperFile
+            );
+
+            if (!is_file($path)) {
+                continue;
+            }
+
+            $function = Str::before($helperFile, '.php');
+
+            if (\function_exists($function)) {
+                continue;
+            }
+
+            require_once $path;
+        }
+
+    }
+}
